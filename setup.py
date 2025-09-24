64130b5a707a1cb1ec43ce163c85191eb76ce169
Siva-43 <ksiva2k2@gmail.com>
2025-09-24 17:56:42 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()  
+        requirements=[i.replace("\n","") for i in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+
+name='mlproject',
+version='0.0.1',
+author='Siva',
+author_email='ksiva2k2@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
